18b7b74aa775975aa3569e68591904f12fd03d9d
mj281408 <155947593+mj281408@users.noreply.github.com>
2024-10-22 10:21:53 +0530

    footer design

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "./css/fonts.css";
 import Header from "./common/header";
+import Footer from "./common/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={`font ${inter.className}`}>
         <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   );
