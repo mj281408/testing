61c8f4464343134b0f3312a160c021488bc0d3bb
dhruvilhitd <147129238+dhruvilhitd@users.noreply.github.com>
2024-11-16 00:55:48 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div className='w-[680px] h-[956.75px] bg-slate-200'>
+      <h1>{`Hello Jara! Start Coding ;}`}</h1>
+    </div>
+    
+  )
+}
+
+export default App
